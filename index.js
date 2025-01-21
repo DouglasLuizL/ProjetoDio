12a49ae8cedb534458f49f3a0bcde52ddb84ba38
DouglasLuizL <douglasluizleandro@gmail.com>
2025-01-21 18:54:33 -0300

    novo

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Obrigado Deus")
+let vitorias = 65;
+let derrotas = 19;
+let resultado = vitorias - derrotas;
+
+function rank(resultado) {
+  console.log("Seu herói está com o saldo de " + resultado);
+  
+  if (resultado <= 10) {
+    console.log("Você está no nível Ferro");
+  } else if (resultado >= 11 && resultado < 20) {
+    console.log("Você está no nível Bronze");
+  } else if (resultado >= 21 && resultado < 50) {
+    console.log("Você está no nível Prata");
+  } else if (resultado >= 51 && resultado < 80) {
+    console.log("Você está no nível Ouro");
+  } else if (resultado >= 81 && resultado < 90) {
+    console.log("Você está no nível Diamante");
+  } else if (resultado >= 91 && resultado < 100) {
+    console.log("Você está no nível Lendário");
+  } else if (resultado >= 101) {
+    console.log("Você está no nível Imortal");
+  }
+}
+
+rank(resultado);
